3d00dbf3bd33ad51653a8108b0a6359a1a3eb53a
HichamGsb <hicham@DESKTOP-6FSQGCK.localdomain>
2022-11-21 20:59:50 +0100

    Premiere version du RecupAbstract

--- fichier.py
+++ fichier.py
@@ -4,7 +4,7 @@ import sys
 # Utilisation : python3 fichier.py dossier_contenant_les_fichiers txt
 
 # Sous dossier contenant les fichier TXT à analyser
-sous_dossier = sys.argv[1]
+# sous_dossier = sys.argv[1]
 
 
 # Récupérer le nom des fichiers du dossier contenant les fichiers en .txt
@@ -54,13 +54,53 @@ def RecupTitle(fichier):
 
 # Récupérer l'abstract d'un fichier
 def RecupAbstract(fichier):
+    keyword = ["Introduction", "INTRODUCTION", "introduction", "Keyword", "keyword", "Keywords", "keywords", "PACS", "pacs"]
     print("Récupération des résumés de l'auteur")
+    word = "Abstract"
+    nb_abstract_line = 0
+    
+    # Chercher la ligne où se situe le mot Abstract
+    file = open(fichier, "r")
+    for line in file:
+        nb_abstract_line += 1
+        if word in line:
+            print("ABSTRACT found ! Line : ")
+            print(nb_abstract_line)
+            break
+    file.close()
+    
+    # Chercher la ligne où se situe le mot KeyWord
+    nb_keyword_line = 0
+    file = open(fichier, "r")
+    found = False
+
+    for line in file:
+        if found == True:
+            break
+        nb_keyword_line += 1
+        for i in range(len(keyword)):
+            if found == True:
+                break
+            if keyword[i] in line:
+                print("KEYWORD found ! Line : ")
+                print(nb_keyword_line)
+                found = True
+
+    with open(fichier, "r") as f:
+        lines = f.readlines()[nb_abstract_line:nb_keyword_line-2]
+        for line in lines:
+            print(line, end="")
+    # nb_abstract_line est maintenant la ligne où le asbtract se situe
+    file.close()
+
     
 def RecupTitlesInATable(path):
     print("Récupération des titres des fichiers txt du dossier "+path+" insérés dans un tableau")
     tableauDesFichiers = os.listdir(path)
     for i in range(len(tableauDesFichiers)) :
-        
+        print("Récupération des titres")
+
+
 
 
 # On parcourt chaque fichier du dossier "Pdftotext"
@@ -72,9 +112,9 @@ def RecupTitlesInATable(path):
 #     f.write()
 
 
-# for (x in tableau_des_PDF):
-    # f = open('')
-    # f.write()
-    # f.close()
+# for x in tableau_des_PDF:
+#     f = open('')
+#     f.write()
+#     f.close()
 
-
+RecupAbstract("test.txt")
