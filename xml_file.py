dd65b4322c1711d4b893758f2e73772791e38500
abdelghanirechidi <ghanipro84@gmail.com>
2022-11-22 15:38:27 +0100

    Implémentatiou fichier xml_file.py : status->reussi / Ce fichier contiens trois fonctions, la premiere permet de mettre le txt dans une variabiable sortie, la deuxieme va utiliser cette variable et la split, ce qui va nous donner un tableau (comme nos infos sont separé par un trais separateur iest donc facile d'inserer ces variables dans notre xml. La derniere fonction generate_xml(s(sortie) va nous generer notre fichier xml (qui portera comme nom le nom du fichier) et va donc insereles valeurs dans leurs balises respectives, le fichier xml final sera ensuite stocker dans un dossier txt2XML

--- xml_file.py
+++ xml_file.py
@@ -1,24 +1,84 @@
 import os
 import sys
 
-Xml_tab = {}
-key = 0
-file = open("exemple.txt")
-for line in file:
-    value = line.split()
-    Xml_tab[key] = value
-    key+=1
+#Xml_tab = {}
+#key = 0
+#file = open("exemple.txt")
+#for line in file:
+#    value = line.split()
+#    Xml_tab[key] = value
+#    key+=1
 
-print(Xml_tab)
+#print(Xml_tab)
 
-print(Xml_tab[0])
+#print(Xml_tab[0])
 
-j=0
-dico_nb = 0
-str = ""
-for i in Xml_tab[dico_nb]:
-    if(i!=Xml_tab[0][0]):
-        str += " " + Xml_tab[dico_nb][j]
-    j+=1
-  
-print(str)
+#j=0
+#dico_nb = 0
+#str = ""
+#for i in Xml_tab[dico_nb]:
+#    if(i!=Xml_tab[0][0]):
+#        str += " " + Xml_tab[dico_nb][j]
+#    j+=1 
+#print(str)
+
+
+# J'assigne le nom de mon fichier à un string
+filename = "exemple.txt"
+
+# Fonction qui ouvre un fichier txt et qui le lit et envoie la sortie
+def open_file(file):
+    with open (file,"r") as f:
+        sortie = f.read()
+    return (sortie)
+
+# Fonction qui va split notre txt et mettre les données separé par les trais, dans un tableau
+def split_file(sortie):
+    sortie = sortie.split("______________________________")
+    return (sortie)
+
+# Fonction qui va generer un fichier xml adapté pour les informations précédemment extraite.
+def generate_xml(sortie):
+    path = "txt2XML/" + sortie[0] + ".xml";
+    with open (path, "w") as f:
+        f.write("<?xml version='1.0' encoding='UTH-8'?>\n")
+        #Balise article
+        f.write("<article>\n")
+        #Balise preamble
+        f.write("\n<preamble>\n")
+        f.write(sortie[0])
+        f.write("</preamble>\n")
+        #Balise titre
+        f.write("\n<titre>")
+        f.write(sortie[1])
+        f.write("</titre>\n")
+        #Balise auteur
+        f.write("\n<auteur>")
+        f.write(sortie[2])
+        f.write("</auteur>\n")
+        #Balise abstract
+        f.write("\n<abstract>")
+        f.write(sortie[3])
+        f.write("</abstract>\n")
+        #Balise biblio
+        f.write("\n<biblio>")
+        f.write(sortie[4])
+        f.write("\n</biblio>\n")
+
+        f.write("\n</article>")
+
+        print("Fichier xml créee !")
+
+
+
+
+# Test :
+#print("-------- Test 1 -------- ")
+sortie = open_file(filename)
+#print(sortie)
+#print("-------- Test 2 -------- ")
+sortie = split_file(sortie)
+#print(sortie[0])
+#print("-------- Test 3 -------- ")
+#Création d'un fichier xml en prenant en argument le tableau sortie (split de notre fichier txt contenant les informations du pdf)
+generate_xml(sortie)
