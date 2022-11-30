5ed8b1a5eb42aa8fc2c1d8094a0fe226502ffe01
WalidMedouaz <walid.medouaz@gmail.com>
2022-11-30 12:17:25 +0100

    Modification des fichiers txt

--- fichier.py
+++ fichier.py
@@ -3,19 +3,19 @@ import sys
 import shutil
 import re
 # Récupérer le nom des fichiers du dossier contenant les fichiers en .txt
-def RecupNamesOfTheTxtFiles(path):
+def recupNamesOfTheTxtFiles(path):
     print("Récupération des titres des PDF dans un tableau")
     tableau_des_TXT = os.listdir(path)                                  # Tableau contenant les nom des fichiers du sous_dossier AVEC le ".txt"
     return tableau_des_TXT
 
-def DeleteDotTxtFromAStringTable(table):
+def deleteDotTxtFromAStringTable(table):
     tableau_des_TXT_sans_le_txt = [""] * len(table)                     # Tableau contenant les nom des fichiers du sous_dossier SANS le ".txt" 
     for i in range(len(table)) :
         tableau_des_TXT_sans_le_txt[i] = table[i][:-4]
     return tableau_des_TXT_sans_le_txt
 
 # Supprimer le dossier Apres_Analyse s'il existe, le créer après vérification
-def CreateAfterDeleteDirectory(folderName: str):
+def createAfterDeleteDirectory(folderName: str):
     if os.path.exists(folderName):
         shutil.rmtree(folderName)
         print(folderName+" directory deleted")
@@ -24,22 +24,30 @@ def CreateAfterDeleteDirectory(folderName: str):
     print(folderName+" directory created")
 
 # Supprimer les espaces des String d'un tableau de String
-def SuppSpacesFromStringTables(stringTable):
+def suppSpacesFromStringTables(stringTable):
     # print("Suppression des espaces des String d'un tableau de String")
     for i in range(len(stringTable)):
-        stringTable[i] = SuppSpacesFromString(stringTable[i])
+        stringTable[i] = suppSpacesFromString(stringTable[i])
     return stringTable
 
 # Supprimer les espaces d'un String
-def SuppSpacesFromString(name: str):
+def suppSpacesFromString(name: str):
     # print("Suppression des espaces d'un String")
     name.replace(" ", "_")
     return name
 
 # Créer tous les fichiers d'un tableau ayant le même nom sans les espaces
-def CreateFileInAFolder(fileName: str, folderName: str):
+def createFileInAFolder(fileName: str, folderName: str):
     print("Création du fichier : "+fileName + "dans le directory :" + folderName)
     # for i in range(len(tableauDesTxtSansLeTxt)):
     nom = fileName+".txt"
     f = open(folderName+"/"+nom, "x")         # Création du fichier dans le dossier "Apres_Analyse"
     f.close()
+
+def recupIntroduction():
+    return 0
+
+path = "Pdftotext"
+tab = recupNamesOfTheTxtFiles(path)
+for files in tab :
+    print(files)
