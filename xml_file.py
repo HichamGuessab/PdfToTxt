35b1869ffc23c34ff42311df1f647a31dbb9759b
WalidMEDOUAZ <walid.medouaz@alumni.univ-avignon.Fr>
2022-12-10 17:13:30 +0100

    Supression des commentaires dans xml_file.py

--- xml_file.py
+++ xml_file.py
@@ -1,28 +1,6 @@
 import os
 import sys
 
-#Xml_tab = {}
-#key = 0
-#file = open("exemple.txt")
-#for line in file:
-#    value = line.split()
-#    Xml_tab[key] = value
-#    key+=1
-
-#print(Xml_tab)
-
-#print(Xml_tab[0])
-
-#j=0
-#dico_nb = 0
-#str = ""
-#for i in Xml_tab[dico_nb]:
-#    if(i!=Xml_tab[0][0]):
-#        str += " " + Xml_tab[dico_nb][j]
-#    j+=1 
-#print(str)
-
-
 # J'assigne le nom de mon fichier à un string
 filename = "exemple.txt"
 
